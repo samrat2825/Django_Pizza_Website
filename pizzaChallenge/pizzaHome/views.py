a97ddff6e3efbb6fba836f9e7f6a4f672aafb023
Samrat Patel <patel.samrat2000@gmail.com>
2021-06-10 19:26:16 +0530

    first commit

--- pizzaChallenge/pizzaHome/views.py
+++ pizzaChallenge/pizzaHome/views.py
@@ -3,10 +3,16 @@ from .models import Pizza
 
 # Create your views here.
 
+
+def product(request):
+
+    pizzas = Pizza.objects.first()
+    print("---------------------------")
+    print(pizzas)
+    return render(request, 'product.html', {'pizzas': pizzas})
+
+
 def index(request):
 
     pizzas = Pizza.objects.all()
-
-    return render(request, 'index.html', {'pizzas':pizzas})
-
-
+    return render(request, 'index.html', {'pizzas': pizzas})
